11bc0ec16012b1c5ba01fdc887d4e503ccea006b
Matheus Valença <82548455+Matheus351@users.noreply.github.com>
2022-09-07 12:49:16 -0300

    Create Regiao.java

---
+++ Regiao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.builder;
+
+/**
+ *
+ * @author matheus
+ */
+public class Regiao {
+    
+    String nome;
+    
+    public Regiao(String nome){
+        this.nome=nome;
+    }
+}
