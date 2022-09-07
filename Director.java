4e8274576cb98ee2f6abb9cc765b2aa72c90fc91
Matheus Valença <82548455+Matheus351@users.noreply.github.com>
2022-09-07 12:49:52 -0300

    Create Director.java

---
+++ Director.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.builder;
+
+/**
+ *
+ * @author matheus
+ */
+public class Director {
+    
+    public void construirClimaRegiao(Builder builder){
+        builder.setTemperatura(30);
+        builder.setRegiao(new Regiao("BRASIL"));
+        builder.setDensidade((float) 1.115);
+        builder.setDiaNublado(true);
+        builder.setPrevisao("Previsão de chuva");
+    }
+    
+    public void constuirClimaCidade(Builder builder){
+        builder.setTemperatura(31);
+        builder.setRegiao(new Regiao("Cajazeiras"));
+        builder.setDensidade((float) 1.119);
+        builder.setDiaNublado(false);
+        builder.setPrevisao("Não há previsão de chuvas");
+    }
+        
+     
+    public void constuirClimaEstado(Builder builder){
+        builder.setTemperatura(28);
+        builder.setRegiao(new Regiao("Paraíba"));
+        builder.setDensidade((float) 1.220);
+        builder.setDiaNublado(true);
+        builder.setPrevisao("Previsão de chuva");
+    }
+       
+}
+
